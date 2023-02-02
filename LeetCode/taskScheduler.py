9595f8c13cc73a35e28f67a3e0ca62b501ffa3cb
dagmawibabi <1babidagi@gmail.com>
2023-02-02 18:29:41 +0300

    some progress

---
+++ LeetCode/taskScheduler.py
@@ -0,0 +1,10 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        counter = Counter(tasks)
+        _, max_count = counter.most_common(1)[0]
+
+        result = (max_count-1) * (n+1)
+        for count in counter.values():
+            if count == max_count:
+                result += 1
+        return max(result, len(tasks))
