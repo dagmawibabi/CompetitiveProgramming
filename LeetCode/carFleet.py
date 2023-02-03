665c2977a1b10665b6363fd82bad9822f832c824
dagmawibabi <1babidagi@gmail.com>
2023-02-03 23:49:37 +0300

    car fleet

---
+++ LeetCode/carFleet.py
@@ -0,0 +1,9 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        times = [float(target-p)/s for p, s in sorted(zip(position, speed))]
+        result, curr = 0, 0
+        for t in reversed(times):
+            if t > curr:
+                result += 1
+                curr = t
+        return result
